65a749f53178b8ac139048818056a34278a35739
ALEXGAVRIKOV45 <alex_gavrikov@rambler.ru>
2022-06-11 17:46:19 +0500

    AvitoTest

---
+++ src/test/java/ru/avito/StepDefs/PageAbstract.java
@@ -0,0 +1,10 @@
+package ru.avito.StepDefs;
+
+public class PageAbstract {
+
+	public PageAbstract() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+	}
+}
+
+
